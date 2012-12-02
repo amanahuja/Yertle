0517789a67b8725c82cec1e5fb08265c44e6f91e
Aman Ahuja <amanahuja@gmail.com>
2012-12-02 02:15:21 -0800

    using template for helloworld

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/<name>'
+def hello(name=None):
+    return render_template('index.html', name=name)
     
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))

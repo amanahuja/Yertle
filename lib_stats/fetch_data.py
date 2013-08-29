af62debe0451b37858a2c10e310a6cc3a594781c
Aman Ahuja <amanahuja@gmail.com>
2013-08-28 18:34:06 -0700

    simplified write to file

--- lib_stats/fetch_data.py
+++ lib_stats/fetch_data.py
@@ -102,11 +102,17 @@ def write_data_to_file(data):
   filepath = os.path.join('data/', filename)
 
   data_keys = data[0].keys()
-
+  
+  '''
   with open(filename, 'wb') as csvfile:
     dict_writer = csv.DictWriter(csvfile, data_keys)
     dict_writer.writer.writerow(data_keys)
 
     for dd in data: 
       dict_writer.writerow(dd)
+  '''
+  with open(filename, 'wb') as outfile: 
+    import pdb; pdb.set_trace()
+    json.dump(data, outfile)
+
 
